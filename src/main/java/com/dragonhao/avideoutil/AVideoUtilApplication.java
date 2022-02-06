e4c3b5243a5ee34a304524feee852542b8ec2114
Zminghao <Zminghao@dragonhao.github.com>
2022-02-07 03:12:04 +0800

    引入SpringBoot框架
    引入SpringBoot框架

---
+++ src/main/java/com/dragonhao/avideoutil/AVideoUtilApplication.java
@@ -0,0 +1,13 @@
+package com.dragonhao.avideoutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AVideoUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AVideoUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dragonhao/avideoutil/AVideoUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragonhao.avideoutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AVideoUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
